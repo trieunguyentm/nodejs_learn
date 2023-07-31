651664fcf24dbe9cc3ba745184cdb7567160cd50
trieunguyentm <trieunguyen241102@gmail.com>
2023-08-01 01:50:32 +0700

    add slug

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
+const slug = require('mongoose-slug-generator')
+
+mongoose.plugin(slug)
 const Schema = mongoose.Schema
 
 const Course = Schema({
     name: { type: String, default: "", maxLength: 255 },
     description: { type: String, default: "", maxLength: 600 },
+    slug: { type: String, slug: ["name"] }
 }, {
     timestamps: true,
 })
