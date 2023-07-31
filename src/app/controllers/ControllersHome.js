18b0d82c20766ac9fc1c125808038a6724558c6a
trieunguyentm <trieunguyen241102@gmail.com>
2023-07-31 21:58:59 +0700

    Controller -> View

--- src/app/controllers/ControllersHome.js
+++ src/app/controllers/ControllersHome.js
@@ -1,8 +1,12 @@
 const Course = require('../models/Course');
-
+const { multipleMongooseToObject } = require("../../utils/mongoose");
 // [GET] url="/"
 exports.home = (req, res, next) => {
   Course.find({})
-    .then(courses => res.json(courses))
+    .then(
+      courses => {
+        res.render('home', { courses: multipleMongooseToObject(courses) })
+      }
+    )
     .catch(next)
 };
